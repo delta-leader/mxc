110032ec4df5e427acb695073025f66f275f7ed8
delta-leader <a0855909@unet.univie.ac.at>
2024-11-01 23:57:31 +0900

    Ma main

--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@
 
 int main(int argc, char* argv[]) {
   MPI_Init(&argc, &argv);
-  typedef std::complex<double> DT;
 
   deviceHandle_t handle;
   ncclComms nccl_comms = nullptr;
-  //cudaSetDevice();
-  //initGpuEnvs(&handle);
+  cudaSetDevice();
+  initGpuEnvs(&handle);
 
   long long Nbody = argc > 1 ? std::atoll(argv[1]) : 2048;
   double theta = argc > 2 ? std::atof(argv[2]) : 1e0;
@@ -31,84 +30,156 @@ int main(int argc, char* argv[]) {
   //Laplace3D eval(1.);
   //Yukawa3D eval(1, 1.);
   //Gaussian eval(0.005);
-  //Helmholtz3D eval(4., 1e-1);
-  Helmholtz3D eval(4., 1.);
+  Helmholtz3D eval(4., 1e-1);
   
   std::vector<double> body(Nbody * 3);
-  //std::vector<std::complex<double>> Xbody(Nbody);
-  MyVector<DT> Xbody(Nbody);
+  std::vector<std::complex<double>> Xbody(Nbody);
   std::vector<Cell> cell(ncells);
 
   //mesh_sphere(&body[0], Nbody, std::sqrt(Nbody / (4 * M_PI)));
-  //uniform_unit_cube_rnd(&body[0], Nbody, 1, 3, 999);
+  uniform_unit_cube_rnd(&body[0], Nbody, 1, 3, 999);
   //uniform_unit_cube(&body[0], Nbody, std::pow(Nbody, 1./3.), 3);
-  mesh_sphere(&body[0], Nbody);
   buildBinaryTree(&cell[0], &body[0], Nbody, levels);
-  Xbody.generate_random(999, -0.5, 0.5);
+
+  std::mt19937 gen(999);
+  std::uniform_real_distribution uniform_dist(0., 1.);
+  std::generate(Xbody.begin(), Xbody.end(), 
+    [&]() { return std::complex<double>(uniform_dist(gen), 0.); });
+
+  /*cell.erase(cell.begin() + 1, cell.begin() + Nleaf - 1);
+  cell[0].Child[0] = 1; cell[0].Child[1] = Nleaf + 1;
+  ncells = Nleaf + 1;
+  levels = 1;*/
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  double h2_construct_time = MPI_Wtime(), h2_construct_comm_time;
+  H2MatrixSolver matA(eval, epi, rank, leveled_rank, cell, theta, &body[0], levels);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  h2_construct_time = MPI_Wtime() - h2_construct_time;
+  h2_construct_comm_time = ColCommMPI::get_comm_time();
+
+  initNcclComms(&nccl_comms, matA.allocedComm);
+  matA.init_gpu_handles(nccl_comms);
+  matA.allocSparseMV(handle, nccl_comms);
+
+  long long lenX = matA.local_bodies.second - matA.local_bodies.first;
+  std::vector<std::complex<double>> X1(lenX, std::complex<double>(0., 0.));
+  std::vector<std::complex<double>> X2(lenX, std::complex<double>(0., 0.));
+
+  std::copy(&Xbody[matA.local_bodies.first], &Xbody[matA.local_bodies.second], &X1[0]);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  double matvec_time = MPI_Wtime(), matvec_comm_time;
+  //matA.matVecMul(&X1[0]);
+  matA.matVecMulSp(handle, &X1[0]);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  matvec_time = MPI_Wtime() - matvec_time;
+  matvec_comm_time = ColCommMPI::get_comm_time();
+
+  double refmatvec_time = MPI_Wtime();
+
+  mat_vec_reference(eval, lenX, Nbody, &X2[0], &Xbody[0], &body[matA.local_bodies.first * 3], &body[0]);
+  refmatvec_time = MPI_Wtime() - refmatvec_time;
+  double cerr = solveRelErr(lenX, &X1[0], &X2[0]);
 
   int mpi_rank = 0, mpi_size = 1;
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-  std::vector<long long> ranks = {32, 64, 96, 128, 160, 192, 224, 256};
-
-  if (mpi_rank == 0)
-    std::cout << Nbody << ", " << leaf_size << ", " << theta << ", " << mode << std::endl;
-  for (size_t i=0; i<ranks.size(); ++i){
-
-  H2MatrixSolver matM;
-  if (mode.compare("h2") == 0)
-    matM = H2MatrixSolver(eval, 0., ranks[i], ranks[i]/2, cell, theta, &body[0], levels);
-  else if (mode.compare("hss") == 0)
-    matM = H2MatrixSolver(eval, 0., ranks[i], ranks[i]/2, cell, 0., &body[0], levels);
-
-  long long lenX = matM.local_bodies.second - matM.local_bodies.first;
-  MyVector<DT> X1(lenX);
-  MyVector<DT> X2(lenX);
-
-  std::copy(&Xbody[matM.local_bodies.first], &Xbody[matM.local_bodies.second], &X1[0]);
-
-  matM.matVecMul(&X1[0]);
-  mat_vec_reference(eval, lenX, Nbody, &X2[0], &Xbody[0], &body[matM.local_bodies.first * 3], &body[0]);
-  double approx_err = solveRelErr(lenX, &X1[0], &X2[0]);
-
-  //initNcclComms(&nccl_comms, matM.allocedComm);
-  //matM.init_gpu_handles(nccl_comms);
-
-  matM.factorizeM();
-  //matM.factorizeDeviceM(handle);
-  std::copy(X2.begin(), X2.end(), X1.begin());
-
-  matM.solvePrecondition(&X1[0]);
-  //matM.solvePreconditionDevice(handle, &X1[0]);
-
-  double ferr = solveRelErr(lenX, &X1[0], &Xbody[matM.local_bodies.first]);
-  MPI_Allgather(&X1[0], lenX, MPI_C_DOUBLE_COMPLEX, &Xbody[0], lenX, MPI_C_DOUBLE_COMPLEX, MPI_COMM_WORLD);
-  double berr = rel_backward_error(eval, lenX, Nbody, &X2[0], &Xbody[0], &body[matM.local_bodies.first * 3], &body[0]);
 
   if (mpi_rank == 0) {
-    std::cout << ranks[i] << ", " << approx_err << ", " << ferr << ", " << berr << std::endl;
-    //std::cout << "Approximation Error: " << approx_err << std::endl;
-    //std::cout << "Forward Error: " << ferr << std::endl;
-    //std::cout << "Backward Error: " << berr << std::endl;
+    std::cout << "Construct Err: " << cerr << std::endl;
+    std::cout << "H^2-Matrix Construct Time: " << h2_construct_time << ", " << h2_construct_comm_time << std::endl;
+    std::cout << "H^2-Matvec Time: " << matvec_time << ", " << matvec_comm_time << std::endl;
+    std::cout << "Dense Matvec Time: " << refmatvec_time << std::endl;
+    /*Eigen::MatrixXcd A(Nbody, Nbody);
+    gen_matrix(eval, Nbody, Nbody, body.data(), body.data(), A.data());
+    double cond = 1. / A.lu().rcond();
+    std::cout << "Condition #: " << cond << std::endl;*/
   }
-  /*
-  std::vector<DT> A(Nbody * Nbody);
-  gen_matrix(*eval, Nbody, Nbody, &body[0], &body[0], A.data());
-  Eigen::Map<const Eigen::Matrix<DT, Eigen::Dynamic, Eigen::Dynamic>> Amap(&A[0], Nbody, Nbody);
-  Eigen::Map<const Eigen::Matrix<DT, Eigen::Dynamic, 1>> bmap(&X2[0], Nbody);
-  Eigen::Map<const Eigen::Matrix<DT, Eigen::Dynamic, 1>> xmap(&X1[0], Nbody);
-  Eigen::Matrix<DT, Eigen::Dynamic, 1> r = bmap- Amap*xmap;
-  std::cout<<Amap.norm()<<" "<<bmap.norm()<<" "<<xmap.norm()<<" "<<r.norm()<<std::endl;
-  std::cout<<r.norm()/ (Amap.norm() * xmap.norm() + bmap.norm()<s)
-  */
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  double m_construct_time = MPI_Wtime(), m_construct_comm_time;
+  H2MatrixSolver matM;
+  if (mode.compare("h2") == 0)
+    matM = H2MatrixSolver(eval, 0., rank, leveled_rank, cell, theta, &body[0], levels);
+  else if (mode.compare("hss") == 0)
+    matM = H2MatrixSolver(eval, 0., rank, leveled_rank, cell, 0., &body[0], levels);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  m_construct_time = MPI_Wtime() - m_construct_time;
+  m_construct_comm_time = ColCommMPI::get_comm_time();
+
+  std::copy(&Xbody[matM.local_bodies.first], &Xbody[matM.local_bodies.second], &X1[0]);
+  matM.matVecMul(&X1[0]);
+  double cerr_m = solveRelErr(lenX, &X1[0], &X2[0]);
+
+  initNcclComms(&nccl_comms, matM.allocedComm);
+  matM.init_gpu_handles(nccl_comms);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  double h2_factor_time = MPI_Wtime(), h2_factor_comm_time;
+
+  //matM.factorizeM();
+  matM.factorizeDeviceM(handle);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  h2_factor_time = MPI_Wtime() - h2_factor_time;
+  h2_factor_comm_time = ColCommMPI::get_comm_time();
+  std::copy(X2.begin(), X2.end(), X1.begin());
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  double h2_sub_time = MPI_Wtime(), h2_sub_comm_time;
+
+  //matM.solvePrecondition(&X1[0]);
+  matM.solvePreconditionDevice(handle, &X1[0]);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  h2_sub_time = MPI_Wtime() - h2_sub_time;
+  h2_sub_comm_time = ColCommMPI::get_comm_time();
+  double serr = solveRelErr(lenX, &X1[0], &Xbody[matM.local_bodies.first]);
+  std::fill(X1.begin(), X1.end(), std::complex<double>(0., 0.));
+
+  if (mpi_rank == 0) {
+    std::cout << "H^2-Preconditioner Construct Time: " << m_construct_time << ", " << m_construct_comm_time << std::endl;
+    std::cout << "H^2-Preconditioner Construct Err: " << cerr_m << std::endl;
+    std::cout << "H^2-Matrix Factorization Time: " << h2_factor_time << ", " << h2_factor_comm_time << std::endl;
+    std::cout << "H^2-Matrix Substitution Time: " << h2_sub_time << ", " << h2_sub_comm_time << std::endl;
+    std::cout << "H^2-Matrix Substitution Err: " << serr << std::endl;
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  double gmres_time = MPI_Wtime(), gmres_comm_time;
+  //matA.solveGMRES(epi, matM, &X1[0], &X2[0], 10, 50);
+  matA.solveGMRESDevice(handle, epi, matM, &X1[0], &X2[0], 10, 50, nccl_comms);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  gmres_time = MPI_Wtime() - gmres_time;
+  gmres_comm_time = ColCommMPI::get_comm_time();
+
+  if (mpi_rank == 0) {
+    std::cout << "GMRES Residual: " << matA.resid[matA.iters] << ", Iters: " << matA.iters << std::endl;
+    std::cout << "GMRES Time: " << gmres_time << ", Comm: " << gmres_comm_time << std::endl;
+    for (long long i = 0; i <= matA.iters; i++)
+      std::cout << "iter "<< i << ": " << matA.resid[i] << std::endl;
+
+    if (csv != nullptr)
+      write_to_csv(csv, mpi_size, Nbody, theta, leaf_size, rank, epi, mode.data(), cerr, 
+        h2_construct_time, h2_construct_comm_time, matvec_time, matvec_comm_time, refmatvec_time, 
+        m_construct_time, m_construct_comm_time, cerr_m, h2_factor_time, h2_factor_comm_time, h2_sub_time, h2_sub_comm_time, serr, 
+        matA.resid[matA.iters], matA.iters, gmres_time, gmres_comm_time, matA.resid.data());
+  }
+
+  matA.free_all_comms();
   matM.free_all_comms();
-  }
   MPI_Finalize();
 
-  //matM.free_gpu_handles();
-  //finalizeGpuEnvs(handle);
-  //finalizeNcclComms(nccl_comms);
-
+  matA.freeSparseMV();
+  matA.free_gpu_handles();
+  matM.free_gpu_handles();
+  finalizeGpuEnvs(handle);
+  finalizeNcclComms(nccl_comms);
   return 0;
 }
 
